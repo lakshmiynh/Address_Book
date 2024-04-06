37aa122f9630bd5f77a642f8c2984189deb2eca4
Lakshmi <lakshmiynh8@gmail.com>
2024-04-06 13:06:14 +0530

    Added:Ability to craete Contact in AddressBook

--- AddressBook.cs
+++ AddressBook.cs
@@ -8,5 +8,27 @@ namespace Address_Book
 {
     internal class AddressBook
     {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Phonenumber { get; set; }
+        public string Email { get; set; }
+
+        public AddressBook(string firstname, string lastname, string address, string city, string state, string zip, string phonenumber, string email)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            Address = address;
+            City = city;
+            State = state;
+            Zip = zip;
+            Phonenumber = phonenumber;
+            Email = email;
+
+        }
     }
 }
+
